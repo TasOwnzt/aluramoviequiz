cf529b9a264e4bd4c6898eb68e1cdc3658fdc64d
TasOwnzt <tas-93@hotmail.com>
2021-01-30 19:34:03 -0300

    Adicionando meta tags ao projeto e o arquivo 'quiz.js'

--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,32 @@
-import styled from 'styled-components'
-import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import Head from 'next/head'
+import QuizHome from './quiz'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
-// const BackgroundImage = styled.div`
-//   background-image: url(${db.bg});
-//   flex: 1;
-//   background-size: cover;
-//   background-position: center;
-// `;
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
-
-export default function Home() {
+function IndexPage() {
   return (
-    <QuizBackground backgroundImage={db.bg}>
-      <QuizContainer>
-        <QuizLogo />
-        <Widget>
-          <Widget.Header>
-            <h1>{db.title}</h1>
-          </Widget.Header>
-          <Widget.Content>
-            <p>{db.description}</p>
-          </Widget.Content>
-        </Widget>
+    <QuizHome>
+      <div>
+        <Head>
+          <title>Quiz Movie</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
 
-        <Widget>
-          <Widget.Content>
-            <h1>Quizes da Galera</h1>
+          <meta name="title" content="Quiz Movie" />
+          <meta name="description" content="Projeto didático desenvolvido durante a Imersão React v2 do Alura" />
 
-            <p>lorem ipsum dolor sit amet...</p>
-          </Widget.Content>
-        </Widget>
-        <Footer />
-      </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/omariosouto" />
-    </QuizBackground>
-  );
+          <meta property="og:title" content="Quiz Movie" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://aluramoviequiz.vercel.app/" />
+          <meta property="og:description" content="Projeto didático desenvolvido durante a Imersão React v2 do Alura" />
+          <meta property="og:image" content="" />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://aluramoviequiz.vercel.app/" />
+          <meta property="twitter:title" content="Quiz Movie" />
+          <meta property="twitter:description" content="Projeto didático desenvolvido durante a Imersão React v2 do Alura" />
+          <meta property="twitter:image" content="" />
+        </Head>
+      </div>
+    </QuizHome>
+  )
 }
+
+export default IndexPage;
